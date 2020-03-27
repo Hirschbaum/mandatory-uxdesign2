0e2e0be286ba705b843a40396a4b4b5b436dba23
adrienn timko <adrienn.timko@gmail.com>
2020-03-27 15:58:15 +0100

    basic quiz

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <header className="mdc-top-app-bar" style={{ backgroundColor: '#424242' }}>
+        <header className="mdc-top-app-bar" style={{ backgroundColor: '#484C7F' }}>
             <div className="mdc-top-app-bar__row">
                 <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
                     <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
-                    <span className="mdc-top-app-bar__title">Quiz Bastards</span>
+                    <span className="mdc-top-app-bar__title">Quiz Master</span>
                 </section>
             </div>
         </header>
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,16 +1,102 @@
 import React from 'react';
 import './Quiz.css';
 import { Helmet } from 'react-helmet';
+import axios from 'axios';
 
-export default class Quiz extends React.Component {
+class Quiz extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quiz: [],
+        };
+    }
+
+    componentDidMount() {
+
+        axios.get('https://opentdb.com/api.php?amount=10', {})
+            .then(response => {
+                let data = response.data.results;
+                console.log(data);
+                this.setState({ quiz: response.data.results })
+            })
+            .catch(error => {
+                console.log('Error while fetching data from API', error);
+            });
+    }
+
     render() {
-        return(
+
+        return (
             <div>
-                <Helmet>
-                    <title>Quiz</title>
-                </Helmet>
-                <h1>Quiz</h1>
+                <div>
+                    <h1>Quiz</h1>
+                    {this.state.quiz.map(x => (
+
+                        <div key={x}>
+
+                            <p> {x.question}</p>
+
+                            <div className="mdc-form-field">
+                                <div className="mdc-radio">
+                                    <input className="mdc-radio__native-control" type="radio" id="x.correct_answer" name={x.question} />
+                                    <div className="mdc-radio__background">
+                                        <div className="mdc-radio__outer-circle"></div>
+                                        <div className="mdc-radio__inner-circle"></div>
+                                    </div>
+                                    <div className="mdc-radio__ripple"></div>
+                                </div>
+                                <label htmlFor={x.correct_answer}>{x.correct_answer}</label>
+                            </div>
+
+                            <div className="mdc-form-field">
+                                <div className="mdc-radio">
+                                    <input className="mdc-radio__native-control" type="radio" id={x.incorrect_answers[0]} name={x.question} />
+                                    <div className="mdc-radio__background">
+                                        <div className="mdc-radio__outer-circle"></div>
+                                        <div className="mdc-radio__inner-circle"></div>
+                                    </div>
+                                    <div className="mdc-radio__ripple"></div>
+                                </div>
+                                <label htmlFor={x.incorrect_answers[0]}>{x.incorrect_answers[0]}</label>
+                            </div>
+
+                            {x.incorrect_answers.length >= 2 ?
+                            <> 
+                            <div className="mdc-form-field">
+                                <div className="mdc-radio">
+                                    <input className="mdc-radio__native-control" type="radio" id={x.incorrect_answers[1]} name={x.question} />
+                                    <div className="mdc-radio__background">
+                                        <div className="mdc-radio__outer-circle"></div>
+                                        <div className="mdc-radio__inner-circle"></div>
+                                    </div>
+                                    <div className="mdc-radio__ripple"></div>
+                                </div>
+                                <label htmlFor={x.incorrect_answers[1]}>{x.incorrect_answers[1]}</label>
+                            </div>
+
+                            <div className="mdc-form-field">
+                                <div className="mdc-radio">
+                                    <input className="mdc-radio__native-control" type="radio" id={x.incorrect_answers[2]} name={x.question} />
+                                    <div className="mdc-radio__background">
+                                        <div className="mdc-radio__outer-circle"></div>
+                                        <div className="mdc-radio__inner-circle"></div>
+                                    </div>
+                                    <div className="mdc-radio__ripple"></div>
+                                </div>
+                                <label htmlFor={x.incorrect_answers[2]}>{x.incorrect_answers[2]}</label>
+                            </div> 
+                            </>
+                            : null }
+
+                        </div>
+
+                    ))}
+
+                </div>
             </div>
         )
     }
 }
+
+export default Quiz;
