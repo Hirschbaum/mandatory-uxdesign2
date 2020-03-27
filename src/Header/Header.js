bd63f7aec771d1dc6aa72244d4638dc3252b4504
adrienn timko <adrienn.timko@gmail.com>
2020-03-27 11:46:54 +0100

    added modal

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <header class="mdc-top-app-bar" style={{ backgroundColor: '#424242' }}>
-            <div class="mdc-top-app-bar__row">
-                <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-                    <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
-                    <span class="mdc-top-app-bar__title">Quiz Bastards</span>
+        <header className="mdc-top-app-bar" style={{ backgroundColor: '#424242' }}>
+            <div className="mdc-top-app-bar__row">
+                <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                    <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
+                    <span className="mdc-top-app-bar__title">Quiz Bastards</span>
                 </section>
             </div>
         </header>
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './Modal.css';
+//import ReactDOM from 'react-dom';
 
-class Modal extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>Modal</h1>
+const Modal = () => {
+   
+return (
+    <div className="modalContainer">
+        <div className="modalBox">
+            <div className="modalHeadline">
+                <h3> Congratulations!</h3>
             </div>
-        )
-    }
-}
+
+            <p>
+                You have answered 10/
+                <span className='correct--answers'>10 </span>
+                correctly!
+            </p>
+            <p>Do you want to start a new quiz?</p>
+
+            <div className="modalsButtonsContainer">
+                <button className="modalButtons">
+                    Cancel
+                </button>
+
+                <button
+                    className="modalButtons blueButtons"
+                >
+                    Restart
+                </button>
+            </div>
+        </div>
+    </div>
+    
+);
+};
 
 export default Modal;
